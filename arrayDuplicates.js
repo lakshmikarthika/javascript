153cc6504538211cd248cd5feac5dcfca07d8b64
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2022-11-28 16:42:16 +0530

    Create arrayDuplicates.js

---
+++ arrayDuplicates.js
@@ -0,0 +1,24 @@
+// Remove duplicates without using built-in
+
+
+let dummyArr = [223,453, 564,567,223,678,564,453];
+
+let clone = [];
+
+function unique(newArr,currentEle){
+    for(var i=0;i<newArr.length;i++){
+        if(newArr[i] == currentEle){
+            return true;
+        }
+    }
+    return false;
+}
+for(var i=0; i<dummyArr.length;i++){
+    if(unique(clone,dummyArr[i])){
+        continue;
+    }
+    else{
+        clone.push(dummyArr[i]);
+    }
+}
+console.log(clone)
