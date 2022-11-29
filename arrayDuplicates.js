85ba9e9bd83d056e1f08f0a76a3d55e7c48f829b
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2022-11-29 11:18:55 +0530

    Update arrayDuplicates.js

--- arrayDuplicates.js
+++ arrayDuplicates.js
@@ -22,3 +22,21 @@ for(var i=0; i<dummyArr.length;i++){
     }
 }
 console.log(clone)
+
+
+
+// Remove duplicates using built-in
+
+let dummyArr = [223,453, 564,567,223,678,564,453,564,223,678,1];
+
+let clone = [];
+
+for(var i=0;i<dummyArr.length;i++){
+    if(clone.includes(dummyArr[i])){
+        continue;
+    }
+    else{
+        clone.push(dummyArr[i])
+    }
+}
+console.log(clone)
