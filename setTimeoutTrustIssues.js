6b54ca77ef739088d3c2a289194cf990b7ab13c4
karthika <49395313+lakshmikarthika@users.noreply.github.com>
2023-12-19 15:04:12 +0530

    Update setTimeoutTrustIssues.js

--- setTimeoutTrustIssues.js
+++ setTimeoutTrustIssues.js
@@ -7,4 +7,10 @@ new1();
 for(let i=1;i<=50000;i++){
     console.log(i);
 }
+
+let startDate = new Date().getTime();    // engaging the main thread for 10 seconds
+let endDate = startDate;
+while(endDate < startDate+10000){
+    endDate = new Date().getTime();
+}
 console.log("Hello");
